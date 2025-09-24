83fefd37bce8aeb9a12846c41f39307e9c31d8d5
Debi Gonzalez <deboragonzalez@alumni.harvard.edu>
2025-09-24 13:19:05 -0400

    Swirl notes on HW3

---
+++ HW3_Notes.R
@@ -0,0 +1,5 @@
+HW 3 Swirl Lesson Notes
+
+| There are two logical values in R, also called boolean values. They are TRUE and FALSE. In R
+| you can construct logical expressions which will evaluate to either TRUE or FALSE.
+
